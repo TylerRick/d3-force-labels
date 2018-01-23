f96f598c8c694f97e0fb5ffee2f3a3810ac8d6cf
Tyler Rick <tyler@tylerrick.com>
2018-01-22 16:07:45 -0800

    Fix: Call updateAnchorPos() *before* starting simulation so that label starts out at anchor position.
    Without this, labels were all starting out clumped together in upper
    left corner and could be seen moving from there when animated.

--- src/d3-force-labels.js
+++ src/d3-force-labels.js
@@ -33,30 +33,31 @@
         if (d && d.__data__) {
           var data = d.__data__
 
-          if (!d.labelPos)  d.labelPos =  {fixed:false}
-          if (!d.anchorPos) d.anchorPos = {fixed:true}
+          if (!d.labelPos)  d.labelPos =  {fixed: false}
+          if (!d.anchorPos) d.anchorPos = {fixed: true}
 
           // Place position objects in __data__ to make them available through
           // d.labelPos/d.anchorPos for different elements
-          data.labelPos = d.labelPos
+          data.labelPos  = d.labelPos
           data.anchorPos = d.anchorPos
 
-          links.push({target:d.anchorPos, source:d.labelPos})
+          links.push({
+            target: d.anchorPos,
+            source: d.labelPos
+          })
           nodes.push(d.anchorPos)
           nodes.push(d.labelPos)
         }
       })
 
-      // Initialize simulation with nodes and some default forces (can be
-      // changed later by user)
+      // Set intial labelPos so that label starts out at anchor position, before starting simulation
+      updateAnchorPos()
+
+      // Initialize simulation with nodes and some default forces (can be changed later by user)
       labelSim
         .nodes(nodes)
         .force("charge", d3.forceManyBody())
-        .force("link", d3.forceLink()
-          .links(links)
-          .distance(function id(link, i) { return 10; })
-        )
-      updateAnchorPos()
+        .force("link",   d3.forceLink().links(links))
     }
 
     // The anchor position should be updated on each tick
